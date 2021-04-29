ab566173b8888588c606a1d58b0573472307748c
daqh <valeriodipasq@gmail.com>
2021-04-29 18:18:20 +0200

    Aggiunta di commenti

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
-const queryParams = new URLSearchParams(window.location.search);
-const expression = queryParams.get('expression');
-if (expression)
-    try {
-        const parsedExpression = math.parse(expression);
-        expressionTex = parsedExpression.toTex({
-            parenthesis: 'keep',
-            implicit: 'hide'
-        });
+// Salvo il valore  del queryString chiamato "expression" in una variabile
+const expression = new URLSearchParams(window.location.search)
+    .get('expression');
 
-        const result = math.derivative(expression, 'x');
+if (expression) // Se la variabile contiene un espressione
+    try {       // Metto il try perché potrebbe non trattarsi di una espressione valida
 
-        const derivativeTex = result.toTex({
-            parenthesis: 'keep',
-            implicit: 'hide'
-        })
+        const expressionTex = math
+            .parse(expression)          // Converto l'espressione in formato testuale in un formato leggibile da mathjs
+            .toTex({                    // Di questo ultimo valore ne ottengo il "Tex", specificando alcune opzioni per la conversione
+                parenthesis: 'keep',
+                implicit: 'hide'
+            });
 
-        $('#result')
+        const derivativeTex = math          // Calcolo la derivata dell'espressione precedente e prendo il Tex anche di questa
+            .derivative(expression, 'x')
+            .toTex({
+                parenthesis: 'keep',
+                implicit: 'hide'
+            })
+
+        $('#result')                        // All'elemento HTML con id result aggiungo una classe per renderlo blu
             .addClass('ui info message')
-            .append("<h3>L'espressione inserita</h3>")
-            .append(MathJax.tex2svg(expressionTex,
+            .append("<h3>L'espressione inserita</h3>")      // Aggiungo un titolo
+            .append(MathJax.tex2svg(expressionTex,          // Converto il Tex dell'espressione in un vettoriale e lo visualizzo
                 {
                     em: 16,
                     ex: 6,
@@ -26,15 +30,15 @@ if (expression)
                 }
             ))
             .append('<div class="ui divider"></div>')
-            .append('<h3>Derivata</h3>')
-            .append(MathJax.tex2svg(derivativeTex,
+            .append('<h3>Derivata</h3>')                    // Aggiungo un altro titolo
+            .append(MathJax.tex2svg(derivativeTex,          // Effettuo la conversione in vettoriale del tex della derivata
                 {
                     em: 16,
                     ex: 6,
                     display: false
                 }
             ))
-    } catch (error) {
+    } catch (error) {                           // Se l'espressione non è corretta stampo un errore
         $('#result')
             .addClass('ui red message')
             .append("<h3>Si è verificato un errore</h3>")
